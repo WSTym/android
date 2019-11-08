de9bf77842f58866b5b05912930f9cc3843a0674
WSTym <serial.0.killer@gmail.com>
2019-11-08 00:37:13 -0300

    Desenvolvimento do layout da tela de login

---
+++ SagaZ/app/src/main/java/com/pdm/sagaz/LoginActivity.java
@@ -0,0 +1,37 @@
+package com.pdm.sagaz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText mEditEmail;
+    private EditText mEditPassword;
+    private Button mBtnEnter;
+    private TextView mTxtNewAccount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mEditEmail = findViewById(R.id.edTxt_Email);
+        mEditPassword = findViewById(R.id.edTxt_Password);
+        mBtnEnter = findViewById(R.id.btn_Enter);
+        mTxtNewAccount = findViewById(R.id.txt_NewAccount);
+
+        mTxtNewAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
