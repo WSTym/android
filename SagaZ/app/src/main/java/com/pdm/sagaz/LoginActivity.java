fc25ac3ca27fd622a998654f1221ce572cfdcf84
WSTym <serial.0.killer@gmail.com>
2019-11-10 01:07:12 -0300

    Integração ao Firebase

--- SagaZ/app/src/main/java/com/pdm/sagaz/LoginActivity.java
+++ SagaZ/app/src/main/java/com/pdm/sagaz/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,6 +26,13 @@ public class LoginActivity extends AppCompatActivity {
         mEditPassword = findViewById(R.id.edTxt_Password);
         mBtnEnter = findViewById(R.id.btn_Enter);
         mTxtNewAccount = findViewById(R.id.txt_NewAccount);
+
+        mBtnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Teste", "Funciona!");
+            }
+        });
 
         mTxtNewAccount.setOnClickListener(new View.OnClickListener() {
             @Override
--- SagaZ/app/src/main/java/com/pdm/sagaz/RegisterActivity.java
+++ SagaZ/app/src/main/java/com/pdm/sagaz/RegisterActivity.java
@@ -1,14 +1,68 @@
 package com.pdm.sagaz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText mEditName;
+    private EditText mEditEmail;
+    private EditText mEditPassword;
+    private Button mBtnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mEditName = findViewById(R.id.edTxt_Name);
+        mEditEmail = findViewById(R.id.edTxt_Email);
+        mEditPassword = findViewById(R.id.edTxt_Password);
+        mBtnSignUp = findViewById(R.id.btn_SignUp);
+
+        mBtnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Teste", "Funciona!");
+            }
+        });
+    }
+
+    private void createUser() {
+        String email = mEditEmail.getText().toString();
+        String password = mEditPassword.getText().toString();
+
+        if (email == null || email.isEmpty() || password == null || password.isEmpty()) {
+            Toast.makeText(this, "O Email e a Senha devem ser preencindos", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.i("Teste", task.getResult().getUser().getUid());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i("Teste", e.getMessage());
+                    }
+                });
     }
 }
+
