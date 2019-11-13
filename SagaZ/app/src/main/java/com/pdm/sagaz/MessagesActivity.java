082835c4c0b6f61072c74815a37f8e5640a498a5
WSTym <serial.0.killer@gmail.com>
2019-11-13 20:56:54 -0300

    Menu na activity de mensagens

--- SagaZ/app/src/main/java/com/pdm/sagaz/MessagesActivity.java
+++ SagaZ/app/src/main/java/com/pdm/sagaz/MessagesActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -23,5 +25,24 @@ public class MessagesActivity extends AppCompatActivity {
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.contacts:
+                break;
+            case R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                verifyAuthentication();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
