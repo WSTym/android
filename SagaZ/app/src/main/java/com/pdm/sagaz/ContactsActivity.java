da4a2c57a30da94088ccf78c2df129c144addf42
WSTym <serial.0.killer@gmail.com>
2019-11-13 23:47:21 -0300

    Continuação da implementação da lista de contatos

--- SagaZ/app/src/main/java/com/pdm/sagaz/ContactsActivity.java
+++ SagaZ/app/src/main/java/com/pdm/sagaz/ContactsActivity.java
@@ -1,14 +1,23 @@
 package com.pdm.sagaz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.squareup.picasso.Picasso;
+import com.xwray.groupie.GroupAdapter;
+import com.xwray.groupie.Item;
+import com.xwray.groupie.ViewHolder;
 
 import java.util.List;
 
@@ -16,10 +25,18 @@ import javax.annotation.Nullable;
 
 public class ContactsActivity extends AppCompatActivity {
 
+    private GroupAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+
+        RecyclerView rv = findViewById(R.id.recycler);
+
+        adapter = new GroupAdapter();
+        rv.setAdapter(adapter);
+        rv.setLayoutManager(new LinearLayoutManager(this));
 
         fetchUsers();
     }
@@ -36,8 +53,36 @@ public class ContactsActivity extends AppCompatActivity {
                         List<DocumentSnapshot> documents = queryDocumentSnapshots.getDocuments();
                         for (DocumentSnapshot doc: documents) {
                             User user = doc.toObject(User.class);
+
+                            adapter.add(new UserItem(user));
                         }
                     }
                 });
+    }
+
+    private class UserItem extends Item<ViewHolder> {
+
+        private final User user;
+
+        private UserItem(User user) {
+            this.user = user;
+        }
+
+        @Override
+        public void bind(@NonNull ViewHolder viewHolder, int position) {
+            TextView txtNameContact = viewHolder.itemView.findViewById(R.id.txt_Contact);
+            ImageView imgPhotoContact = viewHolder.itemView.findViewById(R.id.img_Photo);
+
+            txtNameContact.setText(user.getUsername());
+
+            Picasso.get()
+                    .load(user.getProfileUri())
+                    .into(imgPhotoContact);
+        }
+
+        @Override
+        public int getLayout() {
+            return R.layout.item_user;
+        }
     }
 }
